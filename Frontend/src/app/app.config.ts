c9d4b6e32b0816bc9de57f8cea5cdf9dd3a1018b
TamayoChristian <alemangacho736@gmail.com>
2025-07-05 21:18:17 -0500

    Arreglos en el css

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+//Gracias a estas lineas podemos usar Primerng
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)
+  ]
 };
