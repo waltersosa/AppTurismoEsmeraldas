1703705e193cdba86b9dead5480bc68b0eaefce7
waltersosa <santiagososamejia@gmail.com>
2025-07-06 20:16:56 -0500

    Avance: Lista estructura del backend

--- backend/routes/stats.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import statsController from '../controllers/statsController.js';
-
-const router = express.Router();
-
-router.get('/', statsController.getStats);
-
-export default router; 
