b557e70d39fc34948f51e31efbad6ddff0ff71a7
waltersosa <santiagososamejia@gmail.com>
2025-07-08 16:05:37 -0500

    Avances: Cultura y gastronomia

--- frontend/app-turismo-movil/src/app/pages/home/home.component.ts
+++ frontend/app-turismo-movil/src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { MenuInferiorComponent } from '../menu-inferior/menu-inferior.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuInferiorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ frontend/app-turismo-movil/src/app/pages/menu-inferior/menu-inferior.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu-inferior',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './menu-inferior.component.html',
+  styleUrls: ['./menu-inferior.component.scss']
+})
+export class MenuInferiorComponent {} 
